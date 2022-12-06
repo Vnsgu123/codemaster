87c29264dd2a30d3b4dde3d951b0f4e49a6ecf2e
Vnsgu123 <111989739+Vnsgu123@users.noreply.github.com>
2022-12-07 00:30:28 +0530

    Update task_3a.py

--- task_3a.py
+++ task_3a.py
@@ -170,15 +170,774 @@ def detect_paths_to_graph(image):
 	---
 	paths = detect_paths_to_graph(maze_image)
 	"""    
-
-	paths = {}
-
 	##############	ADD YOUR CODE HERE	##############
+	horizontal_roads_under_construction=[]
+	A1,A2,A3,A4,A5,A6=[],[],[],[],[],[]
+	B1,B2,B3,B4,B5,B6=[],[],[],[],[],[]
+	C1,C2,C3,C4,C5,C6=[],[],[],[],[],[]
+	D1,D2,D3,D4,D5,D6=[],[],[],[],[],[]
+	E1,E2,E3,E4,E5,E6=[],[],[],[],[],[]
+	F1,F2,F3,F4,F5,F6=[],[],[],[],[],[]
+	G1,G2,G3,G4,G5,G6=[],[],[],[],[],[]
+
+	q=100
+	w=200
+	flag=0
+	for j in range(1,6):
+		qq=96
+		ww=105
+			
+		for i in range(1,7):
+			flag=0
+			crop_img=image[qq:ww ,q:w]
+			cv2.imshow('cropimge',crop_img)
+			cv2.waitKey(2)
+			gray = cv2.cvtColor(crop_img,cv2.COLOR_RGB2HSV)
+			mask = cv2.inRange(gray ,(0,0,231),(180,18,255))
+			res = cv2.bitwise_and(crop_img ,crop_img, mask=mask)
+			# cv2.imshow('res image ',res)
+			imgGrey = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+			_, thrash = cv2.threshold(imgGrey, 50, 255, cv2.THRESH_BINARY)
+			# cv2.imshow('thresh image ',thrash)
+			contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+			cv2.waitKey(1)
+			# print(len(contours))
+			# cv2.imshow(" new", img)
+			for contour in contours:
+				approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
+				cv2.drawContours(crop_img, [approx], 0, (0, 0, 255), 2)
+				# cv2.imshow('contour',crop_img)
+				flag=1
+				
+				if j==1 :    
+					u='A'+str(i)
+					z='B'+str(i)
+					v=u+'-'+z
+				elif j==2 :
+					u='B'+str(i)
+					z='C'+str(i)
+					v=u+'-'+z
+				elif j==3 :
+					u='C'+str(i)
+					z='D'+str(i)
+					v=u+'-'+z
+				elif j==4 :
+					u='D'+str(i)
+					z='E'+str(i)
+					v=u+'-'+z
+				elif j==5 :
+					u='E'+str(i)
+					z='F'+str(i)
+					v=u+'-'+z
+				elif j==6 :
+					u='F'+str(i)
+					z='G'+str(i)
+					v=u+'-'+z
+					
+				# print(v)
+				horizontal_roads_under_construction.append(v)
+				# print(list)
+			
+
+			if flag == 0:
+				if j==1 :
+					if i == 1:
+						u='A'+str(i)
+						z='B'+str(i)
+						# q='A'+i
+						# o='B'+i
+						A1.append(z)
+						B1.append(u)
+						print(A1,B1)
+					if i == 2:
+						u='A'+str(i)
+						z='B'+str(i)
+						# q='A'+i
+						# o='B'+i
+						A2.append(z)
+						B2.append(u)
+						print(A2,B2)
+					if i == 3:
+						u='A'+str(i)
+						z='B'+str(i)
+						# q='A'+i
+						# o='B'+i
+						A3.append(z)
+						B3.append(u)
+						print(A3,B3)
+					if i == 4:
+						u='A'+str(i)
+						z='B'+str(i)
+						# q='A'+i
+						# o='B'+i
+						A4.append(z)
+						B4.append(u)
+						print(A4,B4)
+					if i == 5:
+						u='A'+str(i)
+						z='B'+str(i)
+						# q='A'+i
+						# o='B'+i
+						A5.append(z)
+						B5.append(u)
+						print(A5,B5)
+					if i == 6:
+						u='A'+str(i)
+						z='B'+str(i)
+						# q='A'+i
+						# o='B'+i
+						A6.append(z)
+						B6.append(u)
+						print(A6,B6)
+
+				elif j==2 :
+					if i == 1:
+						u='B'+str(i)
+						z='C'+str(i)
+						# q='A'+i
+						# o='B'+i
+						B1.append(z)
+						C1.append(u)
+						print(B1,C1)
+					if i == 2:
+						u='B'+str(i)
+						z='C'+str(i)
+						# q='A'+i
+						# o='B'+i
+						B2.append(z)
+						C2.append(u)
+						print(B2,C2)
+					if i == 3:
+						u='B'+str(i)
+						z='C'+str(i)
+						# q='A'+i
+						# o='B'+i
+						B3.append(z)
+						C3.append(u)
+						print(B3,C3)
+					if i == 4:
+						u='B'+str(i)
+						z='C'+str(i)
+						# q='A'+i
+						# o='B'+i
+						B4.append(z)
+						C4.append(u)
+						print(B4,C4)
+					if i == 5:
+						u='B'+str(i)
+						z='C'+str(i)
+						# q='A'+i
+						# o='B'+i
+						B5.append(z)
+						C5.append(u)
+						print(B5,C5)
+					if i == 6:
+						u='B'+str(i)
+						z='C'+str(i)
+						# q='A'+i
+						# o='B'+i
+						B6.append(z)
+						C6.append(u)
+						print(B6,C6)
+
+				elif j==3 :
+					if i == 1:
+						u='C'+str(i)
+						z='D'+str(i)
+						# q='A'+i
+						# o='C'+i
+						C1.append(z)
+						D1.append(u)
+						print(C1,D1)
+					if i == 2:
+						u='C'+str(i)
+						z='D'+str(i)
+						# q='A'+i
+						# o='C'+i
+						C2.append(z)
+						D2.append(u)
+						print(C2,D2)
+					if i == 3:
+						u='C'+str(i)
+						z='D'+str(i)
+						# q='A'+i
+						# o='C'+i
+						C3.append(z)
+						D3.append(u)
+						print(C3,D3)
+					if i == 4:
+						u='C'+str(i)
+						z='D'+str(i)
+						# q='A'+i
+						# o='C'+i
+						C4.append(z)
+						D4.append(u)
+						print(C4,D4)
+					if i == 5:
+						u='C'+str(i)
+						z='D'+str(i)
+						# q='A'+i
+						# o='C'+i
+						C5.append(z)
+						D5.append(u)
+						print(C5,D5)
+					if i == 6:
+						u='C'+str(i)
+						z='D'+str(i)
+						# q='A'+i
+						# o='C'+i
+						C6.append(z)
+						D6.append(u)
+						print(C6,D6)
+
+				elif j==4:
+					if i == 1:
+						u='D'+str(i)
+						z='E'+str(i)
+						# q='A'+i
+						# o='D'+i
+						D1.append(z)
+						E1.append(u)
+						print(D1,E1)
+					if i == 2:
+						u='D'+str(i)
+						z='E'+str(i)
+						# q='A'+i
+						# o='D'+i
+						D2.append(z)
+						E2.append(u)
+						print(D2,E2)
+					if i == 3:
+						u='D'+str(i)
+						z='E'+str(i)
+						# q='A'+i
+						# o='D'+i
+						D3.append(z)
+						E3.append(u)
+						print(D3,E3)
+					if i == 4:
+						u='D'+str(i)
+						z='E'+str(i)
+						# q='A'+i
+						# o='D'+i
+						D4.append(z)
+						E4.append(u)
+						print(D4,E4)
+					if i == 5:
+						u='D'+str(i)
+						z='E'+str(i)
+						# q='A'+i
+						# o='D'+i
+						D5.append(z)
+						E5.append(u)
+						print(D5,E5)
+					if i == 6:
+						u='D'+str(i)
+						z='E'+str(i)
+						# q='A'+i
+						# o='D'+i
+						D6.append(z)
+						E6.append(u)
+						print(D6,E6)
+
+
+
+				elif j==5 :
+					if i == 1:
+						u='E'+str(i)
+						z='F'+str(i)
+						# q='A'+i
+						# o='E'+i
+						E1.append(z)
+						F1.append(u)
+						print(E1,F1)
+					if i == 2:
+						u='E'+str(i)
+						z='F'+str(i)
+						# q='A'+i
+						# o='E'+i
+						E2.append(z)
+						F2.append(u)
+						print(E2,F2)
+					if i == 3:
+						u='E'+str(i)
+						z='F'+str(i)
+						# q='A'+i
+						# o='E'+i
+						E3.append(z)
+						F3.append(u)
+						print(E3,F3)
+					if i == 4:
+						u='E'+str(i)
+						z='F'+str(i)
+						# q='A'+i
+						# o='E'+i
+						E4.append(z)
+						F4.append(u)
+						print(E4,F4)
+					if i == 5:
+						u='E'+str(i)
+						z='F'+str(i)
+						# q='A'+i
+						# o='E'+i
+						E5.append(z)
+						F5.append(u)
+						print(E5,F5)
+					if i == 6:
+						u='E'+str(i)
+						z='F'+str(i)
+						# q='A'+i
+						# o='E'+i
+						E6.append(z)
+						F6.append(u)
+						print(E6,F6)
+
+				elif j==6 :
+					if i == 1:
+						u='F'+str(i)
+						z='G'+str(i)
+						# q='A'+i
+						# o='F'+i
+						F1.append(z)
+						G1.append(u)
+						print(F1,G1)
+					if i == 2:
+						u='F'+str(i)
+						z='G'+str(i)
+						# q='A'+i
+						# o='F'+i
+						F2.append(z)
+						G2.append(u)
+						print(F2,G2)
+					if i == 3:
+						u='F'+str(i)
+						z='G'+str(i)
+						# q='A'+i
+						# o='F'+i
+						F3.append(z)
+						G3.append(u)
+						print(F3,G3)
+					if i == 4:
+						u='F'+str(i)
+						z='G'+str(i)
+						# q='A'+i
+						# o='F'+i
+						F4.appGnG(z)
+						G4.append(u)
+						print(F4,G4)
+					if i == 5:
+						u='F'+str(i)
+						z='G'+str(i)
+						# q='A'+i
+						# o='F'+i
+						F5.append(z)
+						G5.append(u)
+						print(F5,G5)
+					if i == 6:
+						u='F'+str(i)
+						z='G'+str(i)
+						# q='A'+i
+						# o='F'+i
+						F6.append(z)
+						G6.append(u)
+						print(F6,G6)
+
+
+				
+			qq=qq+100
+			ww=ww+100
+		q=q+100
+		w=w+100
+  	
+	##################################################
+	cv2.imshow('contour',image)
+	cv2.waitKey(0)
+	print(horizontal_roads_under_construction)
+	vertical_roads_under_construction=[]
+	qq=95
+	ww=107
+	for j in range(1,7):
+		q=100
+		w=200
+			
+		for i in range(1,6):
+			flag1=0
+			crop_img=image[q:w ,qq:ww]
+			# cv2.imshow('cropimge',crop_img)
+			cv2.waitKey(2)
+			gray = cv2.cvtColor(crop_img ,cv2.COLOR_RGB2HSV)
+			mask = cv2.inRange(gray ,(0,0,231),(180,18,255))
+			res = cv2.bitwise_and(crop_img ,crop_img, mask=mask)
+			# cv2.imshow('res image ',res)
+			imgGrey = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+			_, thrash = cv2.threshold(imgGrey, 50, 255, cv2.THRESH_BINARY)
+			# cv2.imshow('thresh image ',thrash)
+			contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+			cv2.waitKey(1)
+			# print(len(contours))
+			# cv2.imshow(" new", img)
+			for contour in contours:
+				approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
+				cv2.drawContours(crop_img, [approx], 0, (0, 0, 255), 2)
+				# cv2.imshow('contour',crop_img)
+				k=i
+				l=i+1
+				flag1=1
+				if j==1 :    
+					u='A'+str(k)
+					z='A'+str(l)
+					v=u+'-'+z
+				elif j==2 :
+					u='B'+str(k)
+					z='B'+str(l)
+					v=u+'-'+z
+				elif j==3 :
+					u='C'+str(k)
+					z='C'+str(l)
+					v=u+'-'+z
+				elif j==4 :
+					u='D'+str(k)
+					z='D'+str(l)
+					v=u+'-'+z
+				elif j==5 :
+					u='E'+str(k)
+					z='E'+str(l)
+					v=u+'-'+z
+				elif j==6 :
+					u='F'+str(k)
+					z='F'+str(l)
+					v=u+'-'+z
+				elif j==7 :
+					u='G'+str(k)
+					z='G'+str(l)
+					v=u+'-'+z
+					
+				# print(v)
+				vertical_roads_under_construction.append(v)
+				# print(list)
+			if flag1 == 0:
+				if j==1 :
+					if i == 1:
+						u='A'+str(i)
+						z='A'+str(i+1)
+						# q='A'+i
+						# o='B'+i
+						A1.append(z)
+						A2.append(u)
+						print(A1,A2)
+					if i == 2:
+						u='A'+str(i)
+						z='A'+str(i+1)
+						# q='A'+i
+						# o='B'+i
+						A2.append(z)
+						A3.append(u)
+						print(A2,A3)
+					if i == 3:
+						u='A'+str(i)
+						z='A'+str(i+1)
+						# q='A'+i
+						# o='B'+i
+						A3.append(z)
+						A4.append(u)
+						print(A3,A4)
+					if i == 4:
+						u='A'+str(i)
+						z='A'+str(i+1)
+						# q='A'+i
+						# o='B'+i
+						A4.append(z)
+						A5.append(u)
+						print(A4,B5)
+					if i == 5:
+						u='A'+str(i)
+						z='A'+str(i+1)
+						# q='A'+i
+						# o='B'+i
+						A5.append(z)
+						A6.append(u)
+						print(A5,A6)
+
+				elif j==2 :
+					if i == 1:
+						u='B'+str(i)
+						z='B'+str(i+1)
+						# q='A'+i
+						# o='B'+i
+						B1.append(z)
+						B2.append(u)
+						print(B1,B2)
+					if i == 2:
+						u='B'+str(i)
+						z='B'+str(i+1)
+						# q='A'+i
+						# o='B'+i
+						B2.append(z)
+						B3.append(u)
+						print(B2,B3)
+					if i == 3:
+						u='B'+str(i)
+						z='B'+str(i+1)
+						# q='A'+i
+						# o='B'+i
+						B3.append(z)
+						B4.append(u)
+						print(B3,B4)
+					if i == 4:
+						u='B'+str(i)
+						z='B'+str(i+1)
+						# q='A'+i
+						# o='B'+i
+						B4.append(z)
+						B5.append(u)
+						print(B4,B5)
+					if i == 5:
+						u='B'+str(i)
+						z='B'+str(i+1)
+						# q='A'+i
+						# o='B'+i
+						B5.append(z)
+						print("-----------",B6)
+						B6.append(u)
+						print(B5,B6)
+				elif j==3 :
+					if i == 1:
+						u='C'+str(i)
+						z='C'+str(i+1)
+						# q='A'+i
+						# o='C'+i
+						C1.append(z)
+						C2.append(u)
+						print(C1,C2)
+					if i == 2:
+						u='C'+str(i)
+						z='C'+str(i+1)
+						# q='A'+i
+						# o='C'+i
+						C2.append(z)
+						C3.append(u)
+						print(C2,C3)
+					if i == 3:
+						u='C'+str(i)
+						z='C'+str(i+1)
+						# q='A'+i
+						# o='C'+i
+						C3.append(z)
+						C4.append(u)
+						print(C3,C4)
+					if i == 4:
+						u='C'+str(i)
+						z='C'+str(i+1)
+						# q='A'+i
+						# o='C'+i
+						C4.append(z)
+						C5.append(u)
+						print(C4,C5)
+					if i == 5:
+						u='C'+str(i)
+						z='C'+str(i+1)
+						# q='A'+i
+						# o='C'+i
+						C5.append(z)
+						C6.append(u)
+						print(C5,C6)
+
+				elif j==4:
+					if i == 1:
+						u='D'+str(i)
+						z='D'+str(i+1)
+						# q='A'+i
+						# o='D'+i
+						D1.append(z)
+						D2.append(u)
+						print(D1,D2)
+					if i == 2:
+						u='D'+str(i)
+						z='D'+str(i+1)
+						# q='A'+i
+						# o='D'+i
+						D2.append(z)
+						D3.append(u)
+						print(D2,D3)
+					if i == 3:
+						u='D'+str(i)
+						z='D'+str(i+1)
+						# q='A'+i
+						# o='D'+i
+						D3.append(z)
+						D4.append(u)
+						print(D3,D4)
+					if i == 4:
+						u='D'+str(i)
+						z='D'+str(i+1)
+						# q='A'+i
+						# o='D'+i
+						D4.append(z)
+						D5.append(u)
+						print(D4,D5)
+					if i == 5:
+						u='D'+str(i)
+						z='D'+str(i+1)
+						# q='A'+i
+						# o='D'+i
+						D5.append(z)
+						D6.append(u)
+						print(D5,D6)
+
+				elif j==5 :
+					if i == 1:
+						u='E'+str(i)
+						z='E'+str(i+1)
+						# q='A'+i
+						# o='E'+i
+						E1.append(z)
+						E2.append(u)
+						print(E1,E2)
+					if i == 2:
+						u='E'+str(i)
+						z='E'+str(i+1)
+						# q='A'+i
+						# o='E'+i
+						E2.append(z)
+						E3.append(u)
+						print(E2,E3)
+					if i == 3:
+						u='E'+str(i)
+						z='E'+str(i+1)
+						# q='A'+i
+						# o='E'+i
+						E3.append(z)
+						E4.append(u)
+						print(E3,E4)
+					if i == 4:
+						u='E'+str(i)
+						z='E'+str(i+1)
+						# q='A'+i
+						# o='E'+i
+						E4.append(z)
+						E5.append(u)
+						print(E4,E5)
+					if i == 5:
+						u='E'+str(i)
+						z='E'+str(i+1)
+						# q='A'+i
+						# o='E'+i
+						E5.append(z)
+						E6.append(u)
+						print(E5,E6)
+
+				elif j==6 :
+					if i == 1:
+						u='F'+str(i)
+						z='F'+str(i+1)
+						# q='A'+i
+						# o='F'+i
+						F1.append(z)
+						F2.append(u)
+						print(F1,F2)
+					if i == 2:
+						u='F'+str(i)
+						z='F'+str(i+1)
+						# q='A'+i
+						# o='F'+i
+						F2.append(z)
+						F3.append(u)
+						# print(F2,F3)
+					if i == 3:
+						u='F'+str(i)
+						z='F'+str(i+1)
+						# q='A'+i
+						# o='F'+i
+						F3.append(z)
+						F4.append(u)
+						# print(F3,F4)
+					if i == 4:
+						u='F'+str(i)
+						z='F'+str(i+1)
+						# q='A'+i
+						# o='F'+i
+						F4.append(z)
+						F5.append(u)
+						# print(F4,F5)
+					if i == 5:
+						u='F'+str(i)
+						z='F'+str(i+1)
+						# q='A'+i
+						# o='F'+i
+						F5.append(z)
+						F6.append(u)
+						# print(F5,F6)
+
+
+
+			q=q+100
+			w=w+100
+		qq=qq+100
+		ww=ww+100
+	# print("'vertical_road_under_construction':",end ='')
+	print(vertical_roads_under_construction)
+	cv2.imshow('contour',crop_img)
+	cv2.waitKey(0)	
+	paths={}
+	paths["A1"]=A1
+	paths["A2"]=A2
+	paths["A3"]=A3
+	paths["A4"]=A3
+	paths["A5"]=A3
+	paths["A6"]=A3
+	paths["B1"]=B1
+	paths["B2"]=B2
+	paths["B3"]=B3
+	paths["B4"]=B4
+	paths["B5"]=B5
+	paths["B6"]=B6
+	paths["C1"]=C1
+	paths["C2"]=C2
+	paths["C3"]=C3
+	paths["C4"]=C4
+	paths["C5"]=C5
+	paths["C6"]=C6
+	paths["D1"]=D1
+	paths["D2"]=D2
+	paths["D3"]=D3
+	paths["D4"]=D4
+	paths["D5"]=D5
+	paths["D6"]=D6
+	paths["E1"]=E1
+	paths["E2"]=E2
+	paths["E3"]=E3
+	paths["E4"]=E4
+	paths["E5"]=E5
+	paths["E6"]=E6
+	paths["F1"]=F1
+	paths["F2"]=F2
+	paths["F3"]=F3
+	paths["F4"]=F4
+	paths["F5"]=F5
+	paths["F6"]=F6
+
+
+
+
+
+
+
+
+	# paths=dict({"A1":A1})
+	# paths=dict({"A2":A2})
+	# paths=dict({"A3":A3})
+	# paths=dict({"A4":A4})
+
+	print(paths)
+
+ 
+	# return horizontal_roads_under_construction	
+
+
 	
 	##################################################
 
-	return paths
-
+	# return paths
 
 
 def detect_arena_parameters(maze_image):
@@ -218,6 +977,8 @@ def detect_arena_parameters(maze_image):
 
 	##############	ADD YOUR CODE HERE	##############
 	traffic_signals, start_node, end_node = detect_all_nodes(maze_image)
+	detect_paths_to_graph(maze_image)
+
 	##################################################
 	
 	return arena_parameters
@@ -311,7 +1072,7 @@ if __name__ == "__main__":
 			image = cv2.imread(img_file_path)
 			
 			# detect the arena parameters from the image
-			arena_parameters = detect_arena_parameters(image)
+			arena_parameters = dete ct_arena_parameters(image)
 			print('\n============================================')
 			print("IMAGE: ", file_num)
 			# print(arena_parameters["start_node"], "->>> ", arena_parameters["end_node"] )
